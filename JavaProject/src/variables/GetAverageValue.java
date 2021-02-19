b84602308bfaab584bc0d1c10192f219e6d69da5
admin <admin@YD01-15>
2021-02-19 12:28:29 +0900

    git2

---
+++ JavaProject/src/variables/GetAverageValue.java
@@ -0,0 +1,17 @@
+package variables;
+
+public class GetAverageValue {
+
+	public static void main(String[] args) {
+		int x, y;
+		x = 17;
+		y = 18;
+		
+		double z = x + y;
+		double i = z / 2;
+		System.out.println("두 수의 평균은 : " + i + " 입니다.");
+		
+
+	}
+
+}
---
+++ JavaProject/src/variables/MorningCode.java
@@ -0,0 +1,19 @@
+package variables;
+
+public class MorningCode {
+
+	public static void main(String[] args) {
+		int x = 30;
+		int y = 26;
+		
+		if(y < x) {
+			System.out.println("한창이네요.");
+		} else if ( y == x ) {
+			System.out.println("적정한 나이입니다.");
+		} else {
+			System.out.println("나이가 좀 들었습니다.");
+		}
+
+	}
+
+}
