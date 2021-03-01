696f3db1eccca2cda55981c3f14116e014ceab62
CYS <CYS@DESKTOP-U5IGF7O>
2021-03-01 21:12:51 +0900

    연습문제

--- JavaProject/src/references/ArrayExample1.java
+++ JavaProject/src/references/ArrayExample1.java
@@ -5,13 +5,18 @@ public class ArrayExample1 {
 	public static void main(String[] args) {
 
 		int[] arys = new int[5];
-//
-//		for (int i = 0; i < arys.length; i++) {
-//			int random = (int) ((Math.random()) * 10) + 1;
-//			arys[i] = random;
-//			System.out.println(arys[i]);
-//		}
-
-
+		int max = 0;
+		
+		for (int i = 0; i < arys.length; i++) {
+			int random = (int) ((Math.random()) * 10) + 1;
+			arys[i] = random;
+			System.out.println(arys[i]);
+		}
+		for (int i = 1; i < arys.length; i++) {
+			if (arys[i] > max) {
+				max = arys[i];
+			}
+		}
+		System.out.println("�ִ밪 : " + max);
 	}
 }
